0fa23914ca81221303ba2aae898b1b238b78d515
Elevattd <marianoo.14.md@gmail.com>
2023-06-19 19:00:27 -0300

    agrego configuracion de cors al servidor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { handleRequest } from './routes';
 import { apiKeyMiddleware } from './middlewares/apiKeyMiddleware';
 import { barerTokenMiddleware } from './middlewares/barerTokenMiddleware';
@@ -7,8 +8,15 @@ import { barerTokenMiddleware } from './middlewares/barerTokenMiddleware';
 dotenv.config();
 
 const { PORT } = process.env;
+
+const corsOptions = {
+	origin: '*',
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+};
+
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
 	try {
+		cors(corsOptions);
 		barerTokenMiddleware(req, res, () => {
 			apiKeyMiddleware(req, res, () => {
 				handleRequest(req, res);
