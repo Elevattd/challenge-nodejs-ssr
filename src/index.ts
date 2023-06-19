f6e4fc2015c51ceb86a4f40b6390546d46a2fc3d
Elevattd <marianoo.14.md@gmail.com>
2023-06-19 18:55:12 -0300

    agrego al inicio del servidor que verifique el bearer token

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import http, { IncomingMessage, ServerResponse } from 'http';
 import dotenv from 'dotenv';
 import { handleRequest } from './routes';
 import { apiKeyMiddleware } from './middlewares/apiKeyMiddleware';
+import { barerTokenMiddleware } from './middlewares/barerTokenMiddleware';
 
 dotenv.config();
 
 const { PORT } = process.env;
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
 	try {
-		apiKeyMiddleware(req, res, () => {
-			handleRequest(req, res);
+		barerTokenMiddleware(req, res, () => {
+			apiKeyMiddleware(req, res, () => {
+				handleRequest(req, res);
+			});
 		});
 	} catch (error) {
 		console.log(error);
