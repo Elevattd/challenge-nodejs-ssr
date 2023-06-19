e9077802b97ea2f2eb0323dc80417748bc719127
Elevattd <marianoo.14.md@gmail.com>
2023-06-19 18:43:49 -0300

    implemento middleware que verifique la api key

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 import dotenv from 'dotenv';
 import { handleRequest } from './routes';
+import { apiKeyMiddleware } from './middlewares/apiKeyMiddleware';
 
 dotenv.config();
 
 const { PORT } = process.env;
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
 	try {
-		handleRequest(req, res);
+		apiKeyMiddleware(req, res, () => {
+			handleRequest(req, res);
+		});
 	} catch (error) {
 		console.log(error);
 	}
