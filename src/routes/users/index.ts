8a3aedbdd4c110604138d22598fc27633317a890
Elevattd <marianoo.14.md@gmail.com>
2023-06-19 16:05:34 -0300

    saco funcion y la expongo global, matcheo pathname

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,19 +1,16 @@
-import { IncomingMessage, ServerResponse } from 'http';
 import { usersRoutes } from './usersRoutes';
+import { send404Response } from '../../middlewares/send404Response';
+import { IncomingMessage, ServerResponse } from 'http';
 
 export const usersRouter = async (req: IncomingMessage, res: ServerResponse) => {
 	const { method, url } = req;
+	const { pathname } = new URL(url || '', `http://${req.headers.host}`);
 
-	const route = usersRoutes.find((route) => route.method === method && route.path === url);
+	const route = usersRoutes.find((route) => route.method === method && route.path === pathname);
 
 	if (route) {
 		route.handler(req, res);
 	} else {
 		send404Response(res);
 	}
-};
-
-const send404Response = (res: ServerResponse) => {
-	res.writeHead(404, { 'Content-Type': 'application/json' });
-	res.end(JSON.stringify({ error: 'Ruta no encontrada' }));
 };
